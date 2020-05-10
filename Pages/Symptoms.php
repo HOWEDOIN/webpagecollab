572f636aa53a08c6c9306ec8cf93c240f3ff31a6
howedoin <lambinated@gmail.com>
2020-05-11 06:38:34 +0800

    played with some css

--- Pages/Symptoms.php
+++ Pages/Symptoms.php
@@ -2,27 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Test Title</title>
+    <title>Symptoms of Covid-19</title>
     <link rel="stylesheet" type="text/css" href="css%20stylesheet.css">
-
 </head>
 <body>
 <header>
     <nav>
         <ul>
             <li><a href="Main.php" style="text-align: center">Home</a></li> | <li><a href="Details.php" style="text-align: center">Details about COVID-19</a></li> | <li><a href="Symptoms.php" style="text-align: center">Symptoms of COVID-19</a></li> | <li><a href="History.php" style="text-align: center">History of World-wide Epidemics</a></li> | <li><a href="Statistics.php" style="text-align: center"> Current Statistics of COVID-19 Cases Worldwide</a></li>
-
         </ul>
-        <hr/>
     </nav>
 </header>
-<h1>Introduction to COVID-19</h1>
-
-<p>
-    Move some files around and try to update the navigation to still work!
-</p>
-<p>
-    <em>(hint: you'll need to update the CSS link href as well)</em>
-</p>
 </body>
 </html>
