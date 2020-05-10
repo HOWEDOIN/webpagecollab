94f9a8a02e46228334fdb7329363b419272f0f72
howedoin <lambinated@gmail.com>
2020-05-11 04:41:41 +0800

    refactored all files to php css stylesheet/copysheet still awaiting confirmation to use

---
+++ Pages/Main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Title</title>
+<!--    <link rel="stylesheet" type="text/css" href="css%20stylesheet.css">-->
+
+<!--    <link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&part=1"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&part=2"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&part=3"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&part=4"/><![endif]&ndash;&gt;-->
+    <!--[if lt IE 9]><!--<link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&noMedia=true&part=1"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&noMedia=true&part=2"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&noMedia=true&part=3"/><link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false&noMedia=true&part=4"/><![endif]&ndash;&gt;-->
+<!--    &lt;!&ndash;[if !IE]> &ndash;&gt;<link rel="stylesheet" type="text/css" href="//static1.squarespace.com/static/sitecss/5e77e3e50317543ce8ee1df2/68/5c5a519771c10ba3470d8101/5e77e3e50317543ce8ee1e0d/816-05142015/1587017882443/site.css?&filterFeatures=false"/>-->
+<!--    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+
+    <link rel="stylesheet" href="copysheet.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="Main.php">Home</a></li>
+            <li><a href="Page%202.php">Page 2</a></li>
+            <li><a href="Page%203.php">Page 3</a></li>
+            <li><a href="Page%204.php">Page 4</a></li>
+            <li><a href="Page%205.php">Page 5</a></li>
+        </ul>
+    </nav>
+</header>
+<h1>Home</h1>
+
+<p>
+    Move some files around and try to update the navigation to still work!
+</p>
+<p>
+    <em>(hint: you'll need to update the CSS link href as well)</em>
+</p>
+</body>
+</html>
